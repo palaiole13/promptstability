501f7f321fc7910798ccaee250bf96d4408d3eb9
Elli Palaiologou <elli.palaiologou@gmail.com>
2025-02-25 15:37:57 +0200

    added documentation using sphinx

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Prompt Stability Scores (PSS)'
+copyright = '2025, Christopher Barrie, Elli Palaiologou, Petter Törnberg'
+author = 'Christopher Barrie, Elli Palaiologou, Petter Törnberg'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # to support NumPy/Google style docstrings
+    'sphinx_autodoc_typehints',
+]
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
